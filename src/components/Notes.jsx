5681fce1c41cfcd3ac5ea573c6a0a24ab9b420d8
alaa-abdoh <alaajabdoh1701@gmail.com>
2023-09-30 21:34:57 +0300

    add loader to appear while the data not shown yet

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -2,14 +2,19 @@ import { useEffect, useState } from "react";
 import Note from "./Note";
 function Notes(props) {
  
-  return (
-    <div className="notes">
-        {
-            props.yourNotes.map((note)=>{
-                return <Note key={note._id} nt={note} yourNotes={props.yourNotes} setYourNotes={props.setYourNotes} />
-            })
-        }
-    </div>
+  return (      
+           props.yourNotes.length == 0 ?
+              <div className="loader"><div></div></div>
+            
+           :(
+             <div className="notes">
+              {
+                props.yourNotes.map((note)=>{
+                  return <Note key={note._id} nt={note} yourNotes={props.yourNotes} setYourNotes={props.setYourNotes} />                        
+                })
+              }
+             </div>
+           )        
   );
 }
 export default Notes;
